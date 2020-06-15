a7c06b93aa0ad3a8af6c6a4670a22381ee7d640a
Andrew <aneum7@yandex.com>
2020-06-15 11:23:50 -0400

    modify vanitygaps keybinds to work with stacker

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
 	{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
-	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
+	{ MOD, XK_w,     ACTION##stack, {.i = 0 } }, \
 	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
 	{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
 	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
@@ -98,8 +98,8 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
 	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
 	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      togglegaps,     {0} },		// toggle gaps
-	{ MODKEY|ShiftMask,    		XK_k,      defaultgaps,    {0} },		// reset to default gaps
+	{ MODKEY|ShiftMask,             XK_u,      togglegaps,     {0} },		// toggle gaps
+	{ MODKEY|ShiftMask,    		XK_i,      defaultgaps,    {0} },		// reset to default gaps
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
@@ -132,7 +132,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_BackSpace, quit,        {0} },
+	{ MODKEY|ShiftMask,             XK_q,	   quit,        {0} },
 };
 
 /* button definitions */
